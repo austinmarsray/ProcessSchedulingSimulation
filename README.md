e8c94848f2463a81df7dbdf670a822b6c6f638fa
austinmarsray <austinmray@outlook.com>
2020-11-10 20:40:09 +0800

    add README,generate data randomly

---
+++ README.md
@@ -0,0 +1,4 @@
+to simulate how processes are scheduled in CPU
+![FCFS](./images/FCFS.png)
+![SJF](./images/SJF.png)
+![RR](./images/RR.png)
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from GUI import *
 from strategy import *
-
+import random
 
 if __name__ == "__main__":
-    input = [[3,10,1], [0,5,2], [5,7,3], [6,12,4]]  # [到达时间,服务时间,序列号]
+    input = []
+    # 测试使用的数据
+    # input = [[3,10,1], [0,5,2], [5,7,3], [6,12,4]]  # [到达时间,服务时间,序列号]
+
+    # 随机生成数据
+    r = random.randint(0,4)
+    for i in range(4):
+        if i==r:
+            input.append([0, random.randint(5, 12), i + 1])
+        else:
+            input.append([random.randint(0,10),random.randint(5,12),i+1])
 
     #FCFS调度
     FCFS(input.copy())
